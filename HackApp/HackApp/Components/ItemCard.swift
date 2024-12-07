e047d2d6e83526f64c8c21d73afe0b557b0922f2
Dicky <dn337@cornell.edu>
2024-12-06 23:44:33 -0500

    added new screen

--- HackApp/HackApp/Components/ItemCard.swift
+++ HackApp/HackApp/Components/ItemCard.swift
@@ -5,55 +5,48 @@
 //  Created by Zoey Jin on 12/6/24.
 //
 
+import SwiftUICore
 import SwiftUI
 
 struct ItemCard: View {
     @EnvironmentObject var cartManager: CartManager
     var menuItem: MenuItem
-    
+
     var body: some View {
-        ZStack(alignment: .topTrailing) {
-            
-            ZStack(alignment: .bottom) {
-                Image(menuItem.image)
-                    .resizable()
-                    .cornerRadius(10)
-                    .frame(width: 180)
-                    .scaledToFit()
-                
-                VStack(alignment: .leading){
-                    Text(menuItem.name)
-                        .bold()
-                    Text("$\(menuItem.price)")
-                        .font(.caption)
-                    
-                }
-                .padding()
-                .frame(width: 180, alignment: .leading)
-                .background(.white)
-            }
-            .frame(width: 190, height: 250)
-            .shadow(radius: 3)
-            
-            Button {
-                cartManager.addToCart(menuItem: menuItem)
-            } label: {
-                Image(systemName: "plus")
-                    .padding(10)
-                    .foregroundColor(.black)
+        NavigationLink(destination: DetailView(menuItem: menuItem)) {
+            ZStack(alignment: .topTrailing) {
+                ZStack(alignment: .bottom) {
+                    Image(menuItem.image)
+                        .resizable()
+                        .cornerRadius(10)
+                        .frame(width: 180)
+                        .scaledToFit()
+
+                    VStack(alignment: .leading) {
+                        Text(menuItem.name)
+                            .bold()
+                        Text("$\(menuItem.price)")
+                            .font(.caption)
+                    }
+                    .padding()
+                    .frame(width: 180, alignment: .leading)
                     .background(.white)
-                    .cornerRadius(50)
-//                    .alignmentGuide(.leading, computeValue: { dimension in
-//                            -100
-//                        })
-                    .padding(10)
-                
+                }
+                .frame(width: 190, height: 250)
+                .shadow(radius: 3)
+
+                Button {
+                    cartManager.addToCart(menuItem: menuItem)
+                } label: {
+                    Image(systemName: "plus")
+                        .padding(10)
+                        .foregroundColor(.black)
+                        .background(.white)
+                        .cornerRadius(50)
+                        .padding(10)
+                }
             }
         }
+        .buttonStyle(PlainButtonStyle())
     }
-}
-
-#Preview {
-    ItemCard(menuItem: itemList[0])
-        .environmentObject(CartManager())
 }
